5b0455066cdfbe965ab97a5191df104551074d45
Osama Al Shammari <iwih@outlook.com>
2018-05-22 01:03:42 +0300

    ...

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import "package:flutter/material.dart";
 import "package:flutter_app/category.dart";
 
+const Color _backColor = Colors.green;
+
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     title: "TestApp",
     home: Scaffold(
-      appBar: AppBar(title: Text("TestApp")),
+      appBar: AppBar(
+        title: Text("UNIT CONVERTOR"),
+        backgroundColor: _backColor,
+      ),
       body: Column(children: <Widget>[
         Category(Icon(Icons.add_alarm), "FIRST CAT", Colors.deepOrangeAccent),
         Category(Icon(Icons.ac_unit), "SECOND CAT", Colors.deepOrangeAccent)
